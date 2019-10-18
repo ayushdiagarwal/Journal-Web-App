3808aa0136e9db653c61abed6e792c5bf870ada6
Ayush Agarwal <ayushdiagarwal@gmail.com>
2019-10-18 19:33:43 +0530

    Journal Web APp

--- journal/diary/models.py
+++ journal/diary/models.py
@@ -2,10 +2,19 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth.models import User
+import calendar
+import datetime
+
+date = datetime.date.today()
+current_date = date.strftime("%d %m %Y")
+today = datetime.datetime.strptime(current_date, '%d %m %Y').weekday()
+day = calendar.day_name[today]
+datei = current_date.replace(' ', '/')
+final_date = day[:3] + '. ' + datei
 
 
 class Entry(models.Model):
-    title = models.CharField(max_length=50, default=timezone.now)
+    title = models.CharField(max_length=50, default=final_date)
     date = models.DateTimeField(default=timezone.now)
     content = models.TextField()
     author = models.ForeignKey(
--- journal/journal/settings.py
+++ journal/journal/settings.py
@@ -115,6 +115,7 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+TIME_ZONE = 'Asia/Kolkata'
 
 
 # Static files (CSS, JavaScript, Images)
